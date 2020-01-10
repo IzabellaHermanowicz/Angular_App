ef61ac4a5ad9a08cd6790b34519f9855a9fee34b
IzabellaHe <izabella.hermanowicz@gmail.com>
2020-01-10 17:19:29 +0100

    Adding HTML components with Angular and basic scss style

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { IGallery } from './interface/IGalleries';
+import { Galleries } from './constants/galleries.constant';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'angular';
+  
+  title: string;
+  description: string;
+  galleries: IGallery[];
+
+  constructor(){
+    this.title = 'Moje podróże';
+    this.description = 'Gdzie, kiedy i czemu mnie tam wywiało';
+    this.galleries = Galleries;
+  }
 }
---
+++ angular/src/app/constants/galleries.constant.ts
@@ -0,0 +1,26 @@
+export const Galleries = [{
+    'galleryId': '1',
+    'title': 'Rzym',
+    'dateCreated': '2015-12-15T00:00:00+00:00',
+    'thumbUrl': './assets/img/gallery/1.jpg',
+    'description': 'Kilka dni zwariowanego wypadu do Rzymu.',
+    'tags': [],
+    'photos': []
+}, 
+{
+    'galleryId': '2',
+    'title': 'Maroko',
+    'dateCreated': '2015-08-07T00:00:00+00:00',
+    'thumbUrl': './assets/img/gallery/2.jpg',
+    'description': 'Tydzień zwiedzania południowego Maroka z ojcem.',
+    'tags': [],'photos': []
+}, 
+{
+    'galleryId': '3',
+    'title': 'Tajlandia',
+    'dateCreated': '2014-11-10T00:00:00+00:00',
+    'thumbUrl': './assets/img/gallery/3.jpg',
+    'description': 'Dwa tygodnie wycieczki po Tajlandii.',
+    'tags': [],
+    'photos': []
+}]
