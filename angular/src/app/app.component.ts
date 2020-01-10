9b901ef9c7e75b1156d6b83d9441af10fe78c88d
IzabellaHe <izabella.hermanowicz@gmail.com>
2020-01-10 23:27:35 +0100

    Gallery with input

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IGallery } from './interface/IGalleries';
 import { Galleries } from './constants/galleries.constant';
+import { IfStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +14,12 @@ export class AppComponent {
   title: string;
   description: string;
   galleries: IGallery[];
+  searchValue: string;
 
   constructor(){
     this.title = 'Moje podróże';
     this.description = 'Gdzie, kiedy i czemu mnie tam wywiało';
     this.galleries = Galleries;
+    this.searchValue = '';
   }
 }
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PolishDatePipe } from './pipes/polish-date.pipe';
+import { SearchGalleriesPipe } from './pipes/search-galleries.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PolishDatePipe,
+    SearchGalleriesPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
